182899204f61e644d02c9eee33d46ccbdad510c8
Globegitter <markus.padourek@gmail.com>
2014-01-19 18:14:07 -0500

    Fixed config. Started adding Site content.

--- app/router.js
+++ app/router.js
@@ -1,8 +1,10 @@
 var Router = Ember.Router.extend(); // ensure we don't share routes between all Router instances
 
 Router.map(function() {
-  this.route('component-test');
-  this.route('helper-test');
+  this.route('schedule');
+  this.route('roles');
+  this.route('rules');
+  this.route('contact');
   // this.resource('posts', function() {
   //   this.route('new');
   // });
